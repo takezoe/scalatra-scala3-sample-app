447c1d692f48807b5c461f2b28265c4dd228c86a
Naoki Takezoe <takezoe@gmail.com>
2022-07-18 01:33:19 +0900

    first commit

---
+++ src/main/scala/ScalatraBootstrap.scala
@@ -0,0 +1,7 @@
+import com.example.app._
+import org.scalatra._
+import javax.servlet.ServletContext
+
+class ScalatraBootstrap extends LifeCycle:
+  override def init(context: ServletContext) =
+    context.mount(new MyScalatraServlet, "/*")
---
+++ src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -0,0 +1,8 @@
+package com.example.app
+
+import org.scalatra._
+
+class MyScalatraServlet extends ScalatraServlet:
+  get("/") {
+    views.html.hello()
+  }
